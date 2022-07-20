8e85c77e96ad79bc6d46ee2e1000025c0fe02673
v1nhtage <86281624+v1nhtage@users.noreply.github.com>
2022-07-20 19:10:43 -0400

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,7 +8,7 @@
           <v-stepper-items>
             <v-stepper-content class="mx-n2 mt-n6" step="1">
               <v-card-text class="mt-n5 ml-n4 mt-n2 mt-n2 grey--text">
-                Login in $TARSTRUCK Staff Portal with your credientials. Do not use your Roblox login.
+                Login to $TARSTRUCK Staff Portal with your credientials. Do not use your Roblox login.
               </v-card-text>
               <v-alert type="error" v-if="error" class="mt-n2" color="red">
                 We could not find an account with that username and password.</v-alert
